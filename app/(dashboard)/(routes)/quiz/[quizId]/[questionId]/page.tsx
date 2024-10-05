a1ce1c8157dcb96ee9ab26d08c43cfab64a0df18
Sameer <sameerpd.unofficial@gmail.com>
2024-10-06 00:37:46 +0530

    Fixed UI Responsiveness

--- app/(dashboard)/(routes)/quiz/[quizId]/[questionId]/page.tsx
+++ app/(dashboard)/(routes)/quiz/[quizId]/[questionId]/page.tsx
@@ -79,12 +79,14 @@ export default function Quiz({ params }: { params: { questionId: string } }) {
   }
   return (
     <div className="flex flex-col h-screen gap-8">
-      <div className="px-6 lg:px-12 flex items-center justify-between">
+      <div className="px-6 lg:px-12 flex flex-col sm:flex-row items-center justify-between">
         <div className="flex items-center gap-6">
-          <Link href="/quiz" className="text-5xl">
-            <ChevronLeft size={44} />
+          <Link href="/quiz" className="text-3xl">
+            <ChevronLeft size={34} />
           </Link>
-          <h1 className="text-5xl font-semibold">{quizData.topic}</h1>
+          <h1 className="text-3xl min-[500px]:text-4xl font-semibold">
+            {quizData.topic}
+          </h1>
         </div>
         <QuizProgress
           totalQuestions={quizData.questionIds.length}
@@ -128,17 +130,17 @@ export default function Quiz({ params }: { params: { questionId: string } }) {
               Submit
             </button>
           ) : (
-            <div className="flex items-center justify-between w-full">
-              <div>
+            <div className="flex flex-col min-[420px]:flex-row gap-4 items-center justify-between w-full">
+              <div className="w-full min-[420px]:w-fit text-center">
                 <h1
-                  className={`font-semibold text-2xl uppercase ${
+                  className={`font-semibold text-xl uppercase ${
                     result === 1 ? "text-emerald-600" : "text-red-600"
                   }`}
                 >
                   {result === 1 ? "correct answer" : "wrong answer"}
                 </h1>
                 {result === -1 && (
-                  <h1 className="uppercase text-red-600 text-sm">
+                  <h1 className="uppercase text-red-600 text-xs">
                     correct answer : {questionData.correct_answer}
                   </h1>
                 )}
@@ -146,16 +148,16 @@ export default function Quiz({ params }: { params: { questionId: string } }) {
               <button
                 type="button"
                 onClick={handleNextQues}
-                className="disabled:cursor-not-allowed border-[3px] font-semibold text-emerald-600 text-2xl border-emerald-500 hover:text-white transition-colors duration-150 hover:bg-emerald-500 w-fit px-3 py-2 rounded-xl"
+                className="disabled:cursor-not-allowed flex justify-center border-[3px] font-semibold text-emerald-600 text-lg border-emerald-500 hover:text-white transition-colors duration-150 hover:bg-emerald-500 w-full min-[420px]:w-fit px-3 py-2 rounded-xl"
               >
                 {quizData.questionsAttempted !== quizData.questionIds.length ? (
-                  <span className="flex items-center gap-2">
+                  <div className="flex items-center gap-2">
                     Next Question <ChevronRight size={32} />
-                  </span>
+                  </div>
                 ) : (
-                  <span className="flex items-center gap-2">
+                  <div className="flex items-center gap-2">
                     <ListCheck size={32} /> Finish Test
-                  </span>
+                  </div>
                 )}
               </button>
             </div>
