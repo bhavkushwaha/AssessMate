76c4db40e8f111eb1d423f179dc6b144056e9aea
SubhashSVS <svs.subhash2003@gmail.com>
2024-10-05 19:10:28 +0530

    added modal box

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/components/modal-provider";
 import { QuizProvider } from "@/Context/Store";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ModalProvider />
           <QuizProvider>{children}</QuizProvider>
         </body>
       </html>
