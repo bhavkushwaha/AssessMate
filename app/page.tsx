92360fbe2d9119900b31625af85d8a3c8633c8c5
Bhav Kushwaha <75977991+bhavkushwaha@users.noreply.github.com>
2024-10-05 12:12:56 +0530

    ShadCN Setup Completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    <h1>AssessMate</h1>
+    <h1 className="text-red-500 text-6xl">AssessMate</h1>
    </div>
   );
 }
